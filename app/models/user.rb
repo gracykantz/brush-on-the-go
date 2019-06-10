d58ba3eb8af8e93bff276176d5ec157f8c7c05ca
Mika Bettanin <mauro.bettanin@ikea.com>
2019-06-10 18:24:49 +0200

    nice login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  has_many :products
+  validates :name, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
